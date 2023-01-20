d8529ca7644b954780f33ca81e47265552aba4bf
RommelJuarez <jua22005@byui.edu>
2023-01-19 23:41:34 -0500

    learning 02

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,17 @@
-using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop02 World!");
-    }
-}
+//What is version control and why is it important?
+
+// version control is basically keeping track of changes made to source code
+// of a program we can think of it as a checkpoint, if in the future our program stops working
+// we can always go back to an old version of our code where everything was fine, it is very important to use
+// version control and even more when we work as a team, to make this practice easier there are software
+// version control systems that record all modifications made to a program and make it available
+// for all the people who work with that code, one of them is git that keeps an exact history of each modification
+// within a repository that is where each new modification is stored
+
+// version control helps developers and teams to be efficient, agile and coordinate efforts well
+// from each developer to the same code when using version control software, conflicts between
+// developers to make a change while another developer makes another change, it is useful when we lose our
+// files and we need to recover them since backups are created of each program that we upload to a repository
+// for example, in git we use the "$ git pull" command to push our new modifications to the repository
+// along with a note describing the changes we made.
