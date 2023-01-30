ca41eb87c0b2d46aa12b724f0ff88662dbc21cab
RommelJuarez <jua22005@byui.edu>
2023-01-29 19:09:39 -0500

    week 04

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -15,3 +15,148 @@
 // files and we need to recover them since backups are created of each program that we upload to a repository
 // for example, in git we use the "$ git pull" command to push our new modifications to the repository
 // along with a note describing the changes we made.
+using System;
+using System.IO;
+using System.Globalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.IO;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        string text = "";
+        string hour = DateTime.Now.ToString("HH:mm:ss");
+        
+        
+ 
+            DateTime today = DateTime.Now;
+            
+            CultureInfo culture = new CultureInfo("es-MX");
+            
+            string todayAsStr = today.ToString(culture);
+            
+            string[] fechaYHoraHoy = todayAsStr.Split(" ");
+            
+            string fechaHoy = fechaYHoraHoy[0];
+            
+            
+
+    string fechaParaDiario = fechaHoy.Replace("/", "-"); 
+    
+
+ 
+
+List<string> questions = new List<string>() { "Who was the most interesting person I interacted with today?", 
+                                              "What was the best part of my day?", 
+                                              "How did I see the hand of the Lord in my life today?", 
+                                              "What was the strongest emotion I felt today?", 
+                                              "If I had one thing I could do over today, what would it be?" };
+
+
+
+
+ bool running = true;
+        while (running){
+Console.WriteLine("Please select an option:");
+Console.WriteLine("1. Write");
+Console.WriteLine("2. Display");
+Console.WriteLine("3. Save");
+Console.WriteLine("4. Load");
+Console.WriteLine("5. Quit");
+
+
+
+
+
+int userSelection = int.Parse(Console.ReadLine());
+
+switch (userSelection)
+{
+    case 1:
+        Console.WriteLine("You selected write.");
+        
+        Random rnd = new Random();
+        int randomIndex = rnd.Next(questions.Count);
+        Console.WriteLine(questions[randomIndex]);
+        ///
+        
+            
+            Console.WriteLine("");
+            Console.WriteLine("Write the text:");
+            text = Console.ReadLine();
+            Console.WriteLine("");
+            
+            
+
+            
+
+        break;
+    case 2:
+    Console.WriteLine("Your text is");
+            Console.WriteLine(text);
+
+    break;    
+    case 3:
+        Console.WriteLine("You selected save.");
+        
+              using (StreamWriter sw = new StreamWriter(fechaParaDiario + ".txt"))
+            {
+                
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1. Save in the current directory ");
+               
+                
+
+
+
+
+
+                    int userSelection2 = int.Parse(Console.ReadLine());
+                                    switch (userSelection2){
+                                    case 1:
+                                    Console.WriteLine("You selected Save in the current directory");
+                                    
+                                    
+                                           
+                                           sw.WriteLine(text);
+
+
+                                  
+
+                                    break;
+                                    case 2:
+                                    Console.WriteLine("quit");
+                                    break;
+                                    }
+                
+                
+                
+
+            }
+
+
+        break;
+    case 4:
+        Console.WriteLine("You selected load.");
+        
+        Console.WriteLine("Enter the path of the file to be loaded:");
+                string pathLoad = Console.ReadLine();
+                text = File.ReadAllText(pathLoad);
+                Console.WriteLine("Text from file: " + text);
+                break;
+    case 5:
+            Console.WriteLine("Quit");
+            break;
+
+
+    default:
+        Console.WriteLine("Invalid selection. Please enter 1, 2, or 3.");
+        break;
+}
+}
+    }}
+
